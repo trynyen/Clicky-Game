16348429d1228b2d4808f366fb98710a32fcba38
Trinh Nguyen <blossom90vt@hotmail.com>
2019-07-31 14:25:13 -0500

    fixed deployment

--- src/App.js
+++ src/App.js
@@ -15,16 +15,14 @@ class App extends Component {
     status: "Click an image to begin!"
   };
 
-  
+
   shuffleAndScore = id => {
     // Assign state
     let clickedFriend = this.state.clickedFriend;
-    let score = this.state.score;
-    let topScore = this.state.topScore;
+    // let score = this.state.score;
+    // let topScore = this.state.topScore;
 
-    this.setState({
-      status: "You guessed correctly"
-    });
+
     //If id hasn't got chosen (not in array), push id to array
     //and shuffle cards
     if (clickedFriend.indexOf(id) === -1) {
@@ -46,22 +44,32 @@ class App extends Component {
         score: 0,
         clickedFriend: [],
         status: "You guessed incorrectly"
-
       })
       this.state.friends.sort(() => Math.random() - 1);
     }
 
+    // //If score is higher than top Score, update top score to cscurrent score
+    // if (score > topScore) {
+    //   this.setState({
+    //     topScore: score + 1
+    //   })
+    // }
+  }
+
+  //Everytime user guesses right, add point to score
+  handleIncrement = () => {
+    let score = this.state.score;
+    let topScore = this.state.topScore;
     //If score is higher than top Score, update top score to cscurrent score
     if (score > topScore) {
       this.setState({
         topScore: score + 1
       })
     }
-  }
-
-  //Everytime user guesses right, add point to score
-  handleIncrement = () => {
-    this.setState({ score: this.state.score + 1 });
+    this.setState({
+      status: "You guessed correctly",
+      score: this.state.score + 1
+    });
   };
 
   // Map over this.state.friends and render a FriendCard component for each friend object
@@ -75,8 +83,8 @@ class App extends Component {
         </Navbar>
 
         <Heading>
-          <h3>Clicky Game!</h3>
-          <p>Click on an image to earn points, but don't click on any more than once</p>
+          <h4>Clicky Game!</h4>
+          <h5>Click on an image to earn points, but don't click on any more than once</h5>
         </Heading>
 
         {/* assign value to each card */}
--- src/components/Heading/index.js
+++ src/components/Heading/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./style.css";
 
 function Heading(props) {
-  return <h1 className="title">{props.children}</h1>;
+  return <div className="title">{props.children}</div>;
 }
 
 export default Heading;
