4d01825cb07720b5f7cc7b4cdc7b9f7ab778582c
Trinh Nguyen <blossom90vt@hotmail.com>
2019-07-31 16:45:34 -0500

    fixed some deployment issues

--- src/App.js
+++ src/App.js
@@ -19,25 +19,41 @@ class App extends Component {
   shuffleAndScore = id => {
     // Assign state
     let clickedFriend = this.state.clickedFriend;
-    // let score = this.state.score;
-    // let topScore = this.state.topScore;
+    let score = this.state.score;
+    let topScore = this.state.topScore;
 
 
     //If id hasn't got chosen (not in array), push id to array
-    //and shuffle cards
     if (clickedFriend.indexOf(id) === -1) {
       clickedFriend.push(id);
-      this.handleIncrement();
+
+      // add 1 to score
+      this.setState({
+        status: "You guessed correctly",
+        score: this.state.score + 1
+      });
+
+      //If score is higher than top Score, update top score to current score
+      if (score >= topScore) {
+        this.setState({
+          topScore: score + 1
+        })
+      }
+
+      //Shuffle card
       this.state.friends.sort(() => Math.random() - 1);
+    
     }
+
     //If user has guessed correctly all 12 cards, reset state
     else if (this.state.score === 12) {
       this.setState({
         score: 0,
         clickedFriend: [],
-        status: "You won!!! Click an image to try again"
+        status: "You got all 12 cards!!! Click an image to try again"
       })
     }
+
     //Otherwise, reset state and alert user that they guessed wrong then shuffle cards
     else {
       this.setState({
@@ -45,32 +61,11 @@ class App extends Component {
         clickedFriend: [],
         status: "You guessed incorrectly"
       })
+
+      //Shuffle card
       this.state.friends.sort(() => Math.random() - 1);
     }
-
-    // //If score is higher than top Score, update top score to cscurrent score
-    // if (score > topScore) {
-    //   this.setState({
-    //     topScore: score + 1
-    //   })
-    // }
   }
-
-  //Everytime user guesses right, add point to score
-  handleIncrement = () => {
-    let score = this.state.score;
-    let topScore = this.state.topScore;
-    //If score is higher than top Score, update top score to cscurrent score
-    if (score > topScore) {
-      this.setState({
-        topScore: score + 1
-      })
-    }
-    this.setState({
-      status: "You guessed correctly",
-      score: this.state.score + 1
-    });
-  };
 
   // Map over this.state.friends and render a FriendCard component for each friend object
   render() {
