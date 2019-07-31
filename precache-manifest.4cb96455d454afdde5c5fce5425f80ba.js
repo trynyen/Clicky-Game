0f206492f2ea175ba245bddacd486e27a71883eb
Trinh Nguyen <blossom90vt@hotmail.com>
2019-07-31 14:23:55 -0500

    Updates

---
+++ precache-manifest.4cb96455d454afdde5c5fce5425f80ba.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57bdcb3ef1cbb11095a70668a9af2a18",
+    "url": "/React-Clicky-Game/index.html"
+  },
+  {
+    "revision": "95b141b4fa3c276ad0bc",
+    "url": "/React-Clicky-Game/static/css/main.56b7b394.chunk.css"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/React-Clicky-Game/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "95b141b4fa3c276ad0bc",
+    "url": "/React-Clicky-Game/static/js/main.fafadd71.chunk.js"
+  },
+  {
+    "revision": "339ef2b4a9d9b3bbcf8d",
+    "url": "/React-Clicky-Game/static/js/runtime~main.25e3a6ff.js"
+  }
+]);
